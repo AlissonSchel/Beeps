deb78406a4ab3167063465ec3638f66bed2ad221
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-23 16:57:43 -0300

    Primeiros exemplos class c#

---
+++ Operators/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elementoX = new Teste(5,5);
+
+            elementoX.MostrarInformacoes();
+            elementoX.AlteraInformacoes(5,5);
+            elementoX.AlteraInformacoes();
+            elementoX.MostrarInformacoes();
+
+            Glee(new Teste(5, 5));
+
+            elementoX.MostrarInformacoes();
+
+            Console.ReadKey();
+        }
+        public static void Glee(Teste pTeste)
+        {
+            pTeste.AlteraInformacoes();
+
+            pTeste.MostrarInformacoes();
+        }
+    }
+    public class Teste
+    {
+        public int A { get; set; } = 0;
+        public int B { get; set; } = 0;
+
+        public Teste(int pA, int pB)
+        {
+            A = pA;
+            B = pB;
+        }
+
+        public void MostrarInformacoes()
+        {
+            Console.WriteLine(A + B);
+        }
+
+        public void AlteraInformacoes()
+        {
+            A = B * 100;
+        }
+
+        public void AlteraInformacoes(int pA, int pB)
+        {
+            A = pA * 2;
+            B = pB * 2;
+        }
+    }
+}
